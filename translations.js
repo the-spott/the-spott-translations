cbc5043cc05f0cc560998dacc4bba0424d475130
Dawid Chłodnicki <dawid.chlodnicki@yahoo.com>
2021-03-24 14:04:53 +0100

    Init

--- translations.js
+++ translations.js
@@ -1,3 +1,58 @@
 export default {
-    test: 'Test'
+    'interface.loading': 'Please wait...',
+
+    'interface.join': 'Join',
+    'interface.logout': 'Logout',
+    'interface.sign-in': 'Sign In',
+    'interface.disconnect': 'Disconnect',
+    'interface.back': 'Back',
+    'interface.save': 'Save',
+    'interface.add-session': 'Add new session',
+
+    'interface.sessions': 'Sessions',
+    'interface.participants': 'Participants',
+    'interface.session': 'Video conference',
+    'interface.dashboard': 'Dashboard',
+
+    'interface.password': 'Password',
+    'interface.email': 'Email',
+    'interface.name': 'Name',
+
+    'interface.my-profile': 'My profile',
+    'interface.photo': 'Photo',
+    'interface.first-name': 'First name',
+    'interface.last-name': 'Last name',
+    'interface.date-of-birth': 'Date of birth',
+    'interface.sex': 'Gender',
+    'interface.height': 'Height',
+    'interface.height-unit': 'Height unit',
+    'interface.weight': 'Weight',
+    'interface.weight-unit': 'Weight unit',
+    'interface.resting-heart-rate': 'Resting heart rate',
+
+    'interface.male': 'Male',
+    'interface.female': 'Female',
+    'interface.not-to-say': 'I prefer not to say',
+
+    'interface.centimeters': 'Centimeters',
+    'interface.feet': 'Feet',
+
+    'interface.kilograms': 'Kilograms',
+    'interface.pounds': 'Pounds',
+
+    'interface.table.actions': 'Actions',
+    'interface.table.view': 'View',
+    'interface.table.edit': 'Edit',
+    'interface.table.delete': 'Delete',
+    'interface.message': 'Message',
+    'interface.schedule': 'Schedule',
+    'interface.missed-the-session': '{0} missed the session',
+
+    'interface.trainer-message.placeholder': 'Hi, let\'s have a training tomorrow at 4PM',
+    'interface.trainer-message.title': 'A message from a trainer to',
+    'interface.trainer-message.modal-title': 'Send message to',
+
+    'message.ok': 'Ok!',
+    'interface.clients': 'Clients',
+    'message.login-failed': 'The email address or password you entered is incorrect'
 }
